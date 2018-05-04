dbd254e44bf593015084afd265f1e1d01a98a103
Jonas Hyllengren <jonas.hyllengren@gmail.com>
2018-05-04 19:17:57 +0200

    fun1: Adds solutions 'overload and use string <<' and 'overload and use std::<< explicitly'

--- fun1/fun1.cpp
+++ fun1/fun1.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
+/*
+// Overload char* <<, print with string <<
+ostream& operator<< (ostream& os, const char* str) 
+{
+    return os << string("Initialisation\n") << string(str) << string("Cleanup\n");
+}
+*/
+
+// Call std::<< explicitly
+ostream& operator<< (ostream& os, const char* str) 
+{
+    std::operator<<(os, "Initialisation\n");
+    std::operator<<(os, str);
+    std::operator<<(os, "Cleanup\n");
+    return os;
+}
 
 int main()
 
 {
 
-    std::cout << "Hello!\n";
+    cout << "Hello!\n";
 
 }
